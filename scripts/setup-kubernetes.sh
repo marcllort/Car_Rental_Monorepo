b7342313279cdbbd67df489d0e594cf7e16706c3
marcllort <mac12llm@gmail.com>
2021-01-09 17:00:34 +0100

    Fix scripts

--- scripts/setup-kubernetes.sh
+++ scripts/setup-kubernetes.sh
@@ -2,8 +2,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-controller ingress-nginx/ingress-nginx
 
-kubectl create secret generic firebase-secret --from-file ./api-gateway/car-rental.json
+kubectl create secret generic firebase-secret --from-file ../api-gateway/car-rental.json
 
-
-kubectl apply -f ./k8
-
+kubectl apply -f ../k8
